21dd619002cf1848168307833e1d2bc0fa6d4398
Bence Nagy <bence@underyx.me>
2019-01-10 14:24:27 +0100

    Format code with Black

--- aiohttp_datadog/__init__.py
+++ aiohttp_datadog/__init__.py
@@ -4,7 +4,6 @@ from datadog import DogStatsd
 
 
 class DatadogMiddleware:
-
     def __init__(self, app_prefix, dogstatsd_kwargs=None):
         if dogstatsd_kwargs is None:
             dogstatsd_kwargs = {}
@@ -16,12 +15,14 @@ class DatadogMiddleware:
         return partial(self.middleware, handler)
 
     async def middleware(self, handler, request):
-        with self.dogstatsd.timed('{0}.request.time'.format(self.app_prefix), tags=self.get_tags(request)):
+        with self.dogstatsd.timed(
+            "{0}.request.time".format(self.app_prefix), tags=self.get_tags(request)
+        ):
             return await handler(request)
 
     def get_tags(self, request):
         return [
-            'http_method:{0}'.format(request.method),
-            'http_host:{0}'.format(request.host),
-            'http_path:{0}'.format(request.path),
+            "http_method:{0}".format(request.method),
+            "http_host:{0}".format(request.host),
+            "http_path:{0}".format(request.path),
         ]
